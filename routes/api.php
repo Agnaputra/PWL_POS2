af4e4984ec27ef72ca3862eb85e82d4974abdc92
nanaputra <agenaputra@gmail.com>
2025-04-29 07:33:39 +0700

    praktikum 3 jb 10

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
-
+use App\Http\Controllers\Api\LogoutController;
 
 
 /*
@@ -20,6 +20,8 @@ use App\Http\Controllers\Api\LoginController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 Route:: post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return response()->json([
         'success' => true,
